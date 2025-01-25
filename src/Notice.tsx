9118e545999ceaf93027d6b69ca8f73c4f13f14f
ichikura <shui.1kura@gmail.com>
2025-01-26 00:08:54 +0900

    notice リファクタリング

--- src/Notice.tsx
+++ src/Notice.tsx
@@ -18,11 +18,6 @@ const Notice = () => {
         getNotices();
       }, []);
 
-    //   async function getNotices() {
-    //     const { data }: any | null = await supabase.from("tb_t_notice").select();
-    //     setNotices(data);
-    //   }
-
       async function getNotices() {
         const { data, error }: any | null = await supabase
           .from("tb_t_notice")
@@ -63,68 +58,6 @@ const Notice = () => {
                 </ListItem><Divider variant="inset" component="li" />
             </>
         ))}
-        {/* <ListItem alignItems="flex-start">
-            <ListItemAvatar>
-            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-            </ListItemAvatar>
-            <ListItemText
-            primary="Brunch this weekend?"
-            secondary={
-                <React.Fragment>
-                <Typography
-                    component="span"
-                    variant="body2"
-                    sx={{ color: 'text.primary', display: 'inline' }}
-                >
-                    Ali Connors
-                </Typography>
-                {" — I'll be in your neighborhood doing errands this…"}
-                </React.Fragment>
-            }
-            />
-        </ListItem>
-        <Divider variant="inset" component="li" />
-        <ListItem alignItems="flex-start">
-            <ListItemAvatar>
-            <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-            </ListItemAvatar>
-            <ListItemText
-            primary="Summer BBQ"
-            secondary={
-                <React.Fragment>
-                <Typography
-                    component="span"
-                    variant="body2"
-                    sx={{ color: 'text.primary', display: 'inline' }}
-                >
-                    to Scott, Alex, Jennifer
-                </Typography>
-                {" — Wish I could come, but I'm out of town this…"}
-                </React.Fragment>
-            }
-            />
-        </ListItem>
-        <Divider variant="inset" component="li" />
-        <ListItem alignItems="flex-start">
-            <ListItemAvatar>
-            <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-            </ListItemAvatar>
-            <ListItemText
-            primary="Oui Oui"
-            secondary={
-                <React.Fragment>
-                <Typography
-                    component="span"
-                    variant="body2"
-                    sx={{ color: 'text.primary', display: 'inline' }}
-                >
-                    Sandra Adams
-                </Typography>
-                {' — Do you have Paris recommendations? Have you ever…'}
-                </React.Fragment>
-            }
-            />
-        </ListItem> */}
         </List>
     </div>
   )
